d08aa768a5e39cb3107c50cf91c1f1013831209e
absoluteunit1 <suprun.anton6@gmail.com>
2020-04-09 14:14:52 -0400

    solution apr 9

--- 30DayChallengeApril2020/week2/BackspaceStringCompare_Apr9.py
+++ 30DayChallengeApril2020/week2/BackspaceStringCompare_Apr9.py
@@ -1,14 +1,32 @@
 
 # My solution
-# I used recursion to remove 2 elements (a letter and the following #) at each recursive iteration 
-
-
-def backspace(strs):
-    strs = strs.lstrip("#")
-    n_b = strs.find("#")
-    if n_b == -1:
-        return strs
-    return backspace(strs[:n_b-1] + strs[n_b+1:])
+# I used recursion to remove 2 elements (one letter and its following #) at each recursive call
 
 def backspaceCompare(S, T):
+
+    def backspace(strs):
+        strs = strs.lstrip("#")
+        n_b = strs.find("#")
+        if n_b == -1:
+            return strs
+        return backspace(strs[:n_b-1] + strs[n_b+1:])
+
     return backspace(S) == backspace(T)
+
+# Another solution. 
+# Genius solution. Iterates through the string, and adds letters to a list. 
+# When the char is the # symbol, it rmeoves the last entry from the list.
+# Time: O(M + N), m and n are the lengths of s and t
+# Space: O(M+N)
+def backspaceCompare2(S, T):
+
+    def finalString(s):
+        stack = []
+        for char in s:
+            if stack and char == "#":
+                stack.pop()
+            elif char != "#":
+                stack.append(char)
+        return stack
+    
+    return finalString(S) == finalString(T)
