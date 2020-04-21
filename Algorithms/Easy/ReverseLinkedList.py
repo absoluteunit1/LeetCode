6a38e97d908f3648ac8c3cc41a0bda8eb235fbd2
absoluteunit1 <suprun.anton6@gmail.com>
2020-04-21 13:39:08 -0400

    solution

--- Algorithms/Easy/ReverseLinkedList.py
+++ Algorithms/Easy/ReverseLinkedList.py
@@ -5,13 +5,25 @@
 
 # Iteratively
 
-class Solution:
-    def reverseList(self, head):
-        prev_node = None
-        while head:
-            temp = head
-            head = head.next
-            temp.next = prev_node
-            prev_node = temp
-        return prev_node
+
+def reverseList(head):
+    prev_node = None
+    while head:
+        temp = head
+        head = head.next
+        temp.next = prev_node
+        prev_node = temp
+    return prev_node
+
+# Recursively 
+
+def reverseList2(head):
+    if head == None or head.next == None:
+        return head
+
+    temp = head
+    head = reverseList2(head.next)
+    temp.next.next = temp
+    temp.next = None
+    return head
 
