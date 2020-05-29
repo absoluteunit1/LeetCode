571e8e7a895eea82d9e60010c6f07e4179b71782
absoluteunit1 <suprun.anton6@gmail.com>
2020-05-29 19:02:17 -0400

    solution to 725 (tricky question!)

--- Solutions/LinkedLists/Medium/SplitLinkedListInParts.py
+++ Solutions/LinkedLists/Medium/SplitLinkedListInParts.py
@@ -1,6 +1,30 @@
-# Finish tomorrow
-
 def splitListToParts(root, k):
-    pass
+    cur, l, result = root, 0, [None]*k
+
+    # Get the length of the linked list first
+    while cur:
+        l += 1
+        cur = cur.next
+    extra = l%k
+    elems = l//k
+    dummy = root
+    prev = None
+    for i in range(k):
+        if dummy:
+            result[i] = dummy
+            if extra:
+                parts = elems + 1
+                extra -= 1
+            else:
+                parts = elems
+            # This section is used to iterate through the linked list and break when the the number of elements per that part has been reached
+            for k in range(parts):
+                prev = dummy
+                dummy = dummy.next
+            prev.next = None
+    return result
+
+
+    
 
 
